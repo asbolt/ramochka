460e58e4e7e26be9dec6f7fe81bfb4ed2250611d
asbolt <asboltova@gmail.com>
2025-02-16 13:43:37 +0300

    draw a ramochka, first version

--- ramochka.asm
+++ ramochka.asm
@@ -3,18 +3,19 @@
 .186
 org 100h
 
-Start:  mov ah, 11100010b
+Start:  mov ah, 01100010b
         mov si, offset String
-        mov cx, 5h
+        mov bx, 5h
+        mov bp, 3h
         mov di, 0b800h
         mov es, di
-        mov di, 5 * 80*2 + 40*2
-        call DrawLine
+        mov di, 20 * 80*2 + 40*2
+        call DrawRam
 
         mov ax, 4c00h
         int 21h
 
-String: db '101$'
+String: db '2022 2202$'
 
 ;------------------------------------------------
 ; Draw a line in video meme
@@ -45,6 +46,41 @@ Next:       mov es:[di], ax
 
             mov es:[di], ax
             add di, 2
+
+            ret
+            endp
+
+;------------------------------------------------
+; Draw a ramochka in video mem
+; Entry:    AH    - color attr
+;           DS:SI - addr of 9-byte ASCII seg to draw a frame
+;           BX    - line length
+;           BP    - hight
+;           ES:DI - line beginning addr
+; Exit:     None
+; Destr:    AL CX SI DI DX
+;------------------------------------------------
+DrawRam     proc
+
+            mov cx, bx
+            mov dx, di
+            call DrawLine
+            mov di, dx
+            add di, 80*2
+
+Miu:        mov cx, bx
+            mov dx, di
+            call DrawLine
+            mov cx, bp
+            sub bp, 1
+            sub si, 3
+            mov di, dx
+            add di, 80*2
+            loop Miu
+
+            add si, 3
+            mov cx, bx
+            call DrawLine
 
             ret
             endp
