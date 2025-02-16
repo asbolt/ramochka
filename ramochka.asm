3e1722cf1a28749562ec22cb4ceee9f2679399ae
asbolt <asboltova@gmail.com>
2025-02-16 20:05:29 +0300

    second version

--- ramochka.asm
+++ ramochka.asm
@@ -3,19 +3,32 @@
 .186
 org 100h
 
-Start:  mov ah, 01100010b
-        mov si, offset String
-        mov bx, 5h
-        mov bp, 3h
+Start:  mov ah, 11100010b
+        mov si, offset Symbols
+        mov cx, 0ch
+        mov bx, 3h
         mov di, 0b800h
         mov es, di
-        mov di, 20 * 80*2 + 40*2
+        mov di, 80 - 2
+        sub di, cx
+        add di, 18 * 80*2
+
+        push bx
         call DrawRam
+        pop bx
+
+        mov ah, 11100010b
+        mov di, 5288h
+        mov es, di
+        mov di, offset String
+        mov si, offset String
+        call DrawStr
 
         mov ax, 4c00h
         int 21h
 
-String: db '2022 2202$'
+Symbols: db 'aaaa aaaa$'
+String: db 'pizda$'
 
 ;------------------------------------------------
 ; Draw a line in video meme
@@ -23,29 +36,22 @@ String: db '2022 2202$'
 ;           DS:SI - addr of 3-byte ASCII seg to draw a frame
 ;           CX    - line length
 ;           ES:DI - line beginning addr
+;           DF = 0
 ; Exit:     None
-; Destr:    AL CX SI DI
+; Destr:    AL SI DI
 ;------------------------------------------------
 DrawLine    proc
 
-            mov al, ds:[si]
-            inc si
+            push cx
 
-            mov es:[di], ax
-            add di, 2
+            lodsb
+            stosw
+            lodsb
+            rep stosw
+            lodsb
+            stosw
 
-            mov al, ds:[si]
-            inc si
-
-Next:       mov es:[di], ax
-            add di, 2
-            loop Next
-
-            mov al, ds:[si]
-            inc si
-
-            mov es:[di], ax
-            add di, 2
+            pop cx
 
             ret
             endp
@@ -54,33 +60,82 @@ Next:       mov es:[di], ax
 ; Draw a ramochka in video mem
 ; Entry:    AH    - color attr
 ;           DS:SI - addr of 9-byte ASCII seg to draw a frame
-;           BX    - line length
-;           BP    - hight
+;           CX    - line length
+;           BX    - hight
 ;           ES:DI - line beginning addr
 ; Exit:     None
-; Destr:    AL CX SI DI DX
+; Destr:    AL CX SI DI BX
 ;------------------------------------------------
 DrawRam     proc
 
-            mov cx, bx
-            mov dx, di
+            push di
             call DrawLine
-            mov di, dx
+            pop di
             add di, 80*2
 
-Miu:        mov cx, bx
-            mov dx, di
+            push cx
+
+Next:       pop cx
+            push di
             call DrawLine
-            mov cx, bp
-            sub bp, 1
+            pop di
+            add di, 80*2
+
+            push cx
+            mov cx, bx
+            dec bx
             sub si, 3
-            mov di, dx
-            add di, 80*2
-            loop Miu
 
+            loop Next
+
+
+            pop cx
             add si, 3
-            mov cx, bx
             call DrawLine
+
+            ret
+            endp
+
+;------------------------------------------------
+; Draw a string in video mem
+; Entry:    AH    - color attr
+;           BX    - ramochka hight
+;           DS:SI - addr of string
+;           ES:DI - addr of string
+; Exit:     None
+; Destr:    AL CX SI DI BX
+;------------------------------------------------
+DrawStr     proc
+
+            mov di, si
+            mov al, 24h
+            xor cx, cx
+            dec cx
+            repne scasb
+            neg cx
+            sub cx, 2
+
+            mov di, 0b800h
+            mov es, di
+            mov di, 80*2
+            sub di, cx
+            add di, 18 * 80*2
+
+            push ax
+            mov ax, bx
+            mov bx, 80
+            mul bx
+            add di, ax
+            mov bx, 2
+            mov ax, di
+            div bx
+            mul bx
+            mov di, ax
+            pop ax
+
+Miu:        lodsb
+            stosw
+            loop Miu
 
             ret
             endp
